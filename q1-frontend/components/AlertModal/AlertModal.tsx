a4d87f01ac426c2a8936bd462328bbf4245cf560
audtan125 <117423776+audtan125@users.noreply.github.com>
2023-01-15 13:36:00 +1100

    Add files via upload

---
+++ q1-frontend/components/AlertModal/AlertModal.tsx
@@ -0,0 +1,37 @@
+import { SetStateAction, Dispatch, FormEvent } from "react";
+import { TableContents } from "../Table/Table";
+
+interface AlertModalProps {
+  useContents: Dispatch<SetStateAction<TableContents>>,
+}
+
+export default function AlertModal({useContents}: AlertModalProps) {
+  function onSubmitEvent(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    // hint: the alert given is at (e.target as any).elements[0].value - ignore typescript being annoying
+    const alertValue = (e.target as any).elements[0].value
+    if (alertValue === '') return
+    
+    useContents( 
+      prevTable => {
+      return {
+        ...prevTable, 
+        rowContents: [
+          ...prevTable.rowContents,
+          {
+            alert: alertValue,
+            status: '',
+            updates: []
+          }
+      ]}
+    })
+  }
+  
+  return (
+    <form data-testid='form' onSubmit={onSubmitEvent}>
+      <label> Add new alert: </label>
+      <input type='text' id='alert' name='alert' />
+      <button type='submit'> Add </button>
+    </form>
+  )
+}
